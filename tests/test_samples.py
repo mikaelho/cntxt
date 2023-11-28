32854e036f8780569da218cd738057219a17cba8
Mikael Honkala <mhonkala@holvi.com>
2023-11-28 17:59:35 +0200

    Refine Animation sample test

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -2,7 +2,9 @@ from samples.animation_parameters import Animation
 from samples.animation_parameters import EASE_OUT_IN
 from samples.animation_parameters import duration
 from samples.animation_parameters import ease
+from samples.animation_parameters import start_delay
 from samples.plugins.configured import core_function
+
 
 def test_plugins():
     assert core_function() == ["main", "plugin"]
@@ -15,8 +17,9 @@ def test_animation():
 
         # Introduce some graphical elements here, use Animation to provide animation properties
 
-        with duration(1.0):
+        with duration(1.0), start_delay():
+            assert Animation.start_delay == 0.3
             assert Animation.duration == 1.0
             assert Animation.ease == EASE_OUT_IN
 
-            # Introduce other elements here, with a slower movement but consistent ease function
+            # Introduce other elements here, with some parameters changed but consistent ease function
